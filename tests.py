fec8d23e8b35b723cffb788dff022e4de045aa2e
xiaojueguan <xiaojueguan@gmail.com>
2019-10-28 17:34:28 +0800

    Make tox and unittest for python3 functionalble

---
+++ tests.py
@@ -0,0 +1,13 @@
+import random
+import unittest
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    """ This is one of potentially many TestCases """
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
