51d360fe86a24a23f9d7c15a9d640b1b9f272f25
Xiaojue Guan <xiaojueguan@Xiaojues-MacBook-Pro.local>
2020-07-29 19:21:04 +0800

    improve configuration in setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,35 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="X-BLOG",
     version="0.1",
+    packages=find_packages(),
+    scripts=[],
+
+    install_requires=[],
+
+    package_data={
+
+    }
+
     author="xiaojueguan",
     author_email="xiaojueguan@gmail.com",
     description="A blog web application package",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="blog django python",
     url="https://github.com/albertjone/x-blog",
-    packages=setuptools.find_packages(),
+    project_urls = {
+        "Bug Tracker": "https://github.com/albertjone/x-blog/issues",
+        "Documentation": "https://github.com/albertjone/x-blog/blob/master/readme.md",
+        "Source Code": "https://github.com/albertjone/x-blog"
+    }
+
     classifiers=[
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
